0ddff41990000dbd90e6a789110f955f43f76892
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-05 15:54:48 +0500

    sidebar and navbar ui

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-3xl text-sky-600">
-      <UserButton afterSignOutUrl="/"/>
       <>this is protected route</>
     </div>
   )
