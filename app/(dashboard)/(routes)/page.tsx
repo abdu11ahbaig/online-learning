a2c00a24c0de2cd0dbe9a4acf627867551ff6548
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-05 13:40:11 +0500

    authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-3xl text-sky-600">
-      learning plate form
+      <UserButton afterSignOutUrl="/"/>
+      <>this is protected route</>
     </div>
   )
 }
