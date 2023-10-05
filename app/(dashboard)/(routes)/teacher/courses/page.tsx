08ffc470ae60cf765a9c119394596a401cfaa651
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-05 18:09:43 +0500

    build course setup ui

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
-  return ( <div>courses page</div> );
+  return (
+  <Link href="/teacher/create">
+    <div className="p-6">
+      <Button>add new course</Button>
+    </div>
+  </Link>
+  );
 }
 
 export default CoursesPage;
