3a288e4413c9f469c36a38abbcc3cfd292ed8446
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-09 13:42:03 +0500

    added course description section

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -13,31 +13,33 @@ FormField,
 FormItem,
 FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Pencil } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
 
 
 
-interface TitleFormProps{
+interface DescriptionFormProps{
   initialData:{
-    title: string;
+    description: string;
   };
   courseId: string;
 }
 
 const formSchema = z.object({
-  title: z.string().min(1 ,{
-    message:"Title is required",
+  description: z.string().min(1 ,{
+    message:"Description is required",
   }),
 });
 
-export const TitleForm = ({
+export const DescriptionForm = ({
   initialData,
   courseId
-}: TitleFormProps) => {
+}: DescriptionFormProps) => {
   const [isEditing, setIsEditing]  = useState(false);
 
   const toggleEdit = () => setIsEditing((current) => !current);
@@ -62,14 +64,14 @@ export const TitleForm = ({
   return(
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
        <div className="font-medium flex items-center justify-between">
-        Course Title
+        Course description
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ):(
               <>
                 <Pencil className="h-4 w-4 mr-2"/>
-                Edit title
+                Edit description
               </>
             )
           }
@@ -77,7 +79,9 @@ export const TitleForm = ({
         </Button>
        </div>
        {!isEditing &&
-        (<p className="text-sm mt-2">{initialData.title}</p>)}
+        (<p className={cn(
+          "text-sm mt-2", !initialData.description && "text-slate-500 italic"
+        )}>{initialData.description || " No description"}</p>)}
         {isEditing && (
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}
@@ -85,11 +89,11 @@ export const TitleForm = ({
             >
               <FormField
               control={form.control}
-              name="title"
+              name="description"
               render={({field}) => (
                 <FormItem>
                   <FormControl>
-                    <Input disabled= {isSubmitting} placeholder="e.g 'advance web dev course'"
+                    <Textarea disabled= {isSubmitting} placeholder="e.g. 'This course is about ....'"
                     {...field}
                     />
                   </FormControl>
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 // import { boolean } from "zod";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({
   params
@@ -58,6 +59,10 @@ const CourseIdPage = async ({
             <h2 className="text-xl">Customize your course</h2>
           </div>
           <TitleForm
+          initialData= {course}
+          courseId= {course.id}
+          />
+          <DescriptionForm
           initialData= {course}
           courseId= {course.id}
           />
